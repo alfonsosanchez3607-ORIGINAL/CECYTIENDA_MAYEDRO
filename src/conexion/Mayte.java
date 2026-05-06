af54eddc12695dfffb9e5c82bd68f964524994ff
maytevidalmmcdr-cloud <maytevidal@gmail.com>
2026-05-05 18:14:32 -0600

    Mayte agrego algo att Mayte

---
+++ src/conexion/Mayte.java
@@ -0,0 +1,48 @@
+package conexion;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Mayte {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Mayte window = new Mayte();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Mayte() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("MAYTE");
+		lblNewLabel.setBounds(21, 37, 325, 119);
+		frame.getContentPane().add(lblNewLabel);
+	}
+}
